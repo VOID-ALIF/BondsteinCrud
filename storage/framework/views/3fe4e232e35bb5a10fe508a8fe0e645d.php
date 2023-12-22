4bd970df1b81aeffc78e906fbc857907f4486cee
ALIF RAHMAN <54330425+VOID-ALIF@users.noreply.github.com>
2023-12-23 00:53:20 +0600

    Add files via upload

---
+++ storage/framework/views/3fe4e232e35bb5a10fe508a8fe0e645d.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title style="justify-content: center">Bondstein CRUD by ALif Rahman</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #333;
+            color: #fff;
+        }
+        .container {
+            margin-top: 50px;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+    <?php echo $__env->yieldContent('content'); ?>
+</div>
+
+</body>
+</html>
+<?php /**PATH E:\laragon\www\BondsteinCrud\resources\views/products/layout.blade.php ENDPATH**/ ?>
